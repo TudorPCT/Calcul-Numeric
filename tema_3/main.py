ab4fe7876bb7950f26ae812109f5697fbbc9b61c
TudorPCT <tcosmin.pasat@gmail.com>
2023-03-16 22:55:22 +0200

    4-5-6

--- tema_3/main.py
+++ tema_3/main.py
@@ -8,25 +8,26 @@ epsilon = 10 ** -9
 
 
 def get_b(a, s):
+    # b = np.zeros(len(a), dtype=np.float64)
+    # for i in range(len(a)):
+    #     for j in range(len(a)):
+    #         b[i] = b[i] + a[i][j] * s[j]
     return a @ s
 
 
 def householder(a, n, b):
     q = np.identity(n, dtype=np.float64)
-    # u = np.zeros(n, dtype=np.float64)
-    b_init = b.copy()
-    a_init = a.copy()
-    beta = 0
-    k = 0
     for r in range(0, n - 1):
         sigma = 0
         for i in range(r, n):
             sigma = sigma + pow(a[i][r], 2)
         if sigma <= epsilon:
             break
+
         k = sqrt(sigma)
-        if a[r][r] > 0:
+        if a[r][r] >= 0:
             k = -k
+
         beta = sigma - k * a[r][r]
 
         u = np.zeros(n, dtype=np.float64)
@@ -45,6 +46,8 @@ def householder(a, n, b):
         a[r][r] = k
         for i in range(r + 1, n):
             a[i][r] = 0
+
+        # b = Pr*b
         s = 0
         for i in range(r, n):
             s = s + u[i] * b[i]
@@ -52,6 +55,7 @@ def householder(a, n, b):
         for i in range(r, n):
             b[i] = b[i] - gamma * u[i]
 
+        # Q = Q*Pr
         for j in range(0, n):
             s = 0
             for i in range(r, n):
@@ -60,15 +64,13 @@ def householder(a, n, b):
             for i in range(r, n):
                 q[i][j] = q[i][j] - gamma * u[i]
 
-    return q.transpose(), a
+    return q.transpose()
 
 
 def solve_system(a, b, n):
-    print(a)
-    print(b)
+    # print(a)
+    # print(b)
     x = np.zeros(len(a), dtype=np.float64)
-    # for i in range(len(a)-1, -1, -1):
-    #     x[i] = b[i] - sum(a[i][j] * x[j] for j in range(i))
 
     for i in range(n - 1, -1, -1):
         x[i] = (b[i] - sum(a[i][j] * x[j] for j in range(i + 1, n))) / a[i][i]
@@ -76,13 +78,34 @@ def solve_system(a, b, n):
     return x.transpose()
 
 
-def norm_check(a, b):
-    norm_v = a - b
+def solve_householder(a, s):
+    b = get_b(a, s)
+    q = householder(a, len(a), b)
+    return solve_system(a, b, len(a))
+
+
+def compute_norm(norm_v):
     norm = 0
     for i in norm_v:
-        norm = norm + i*i
+        norm = norm + i * i
     norm = math.sqrt(norm)
-    return np.alltrue(norm <= epsilon)
+    return norm
+
+
+def compute_inv(a):
+    a_inv = np.empty((len(a), 0), dtype=np.float64)
+    _a = np.copy(a)
+    q = householder(_a, len(_a), get_b(_a, np.identity(len(_a), dtype=np.float64)))
+    if np.alltrue(abs(np.diag(_a)) < epsilon):
+        print("Matrix is singular")
+        return None
+
+    for j in range(len(a)):
+        b = q[j]
+        x = solve_system(_a, b, len(a))
+        a_inv = np.column_stack((a_inv, x))
+
+    return a_inv
 
 
 if __name__ == '__main__':
@@ -95,21 +118,64 @@ if __name__ == '__main__':
     # input_b = np.array([[60], [80], [75]], dtype=np.float64)
     print("Initial system:")
     print("A:\n", input_a)
+    print("=====================================")
+    print("Exercise 1")
     print("b:\n", input_b)
+    print("=====================================")
     print("s:\n", input_s)
-    q, r = householder(input_a, 3, input_b)
-    print("Q:\n", q)
-    print("R:\n", r)
 
+    print("=====================================")
+    print("Exercise 2")
+    _q = householder(input_a, 3, input_b)
+    print("Q:\n", _q)
+    print("R:\n", input_a)
+    print("=====================================")
+
+    print("Exercise 3")
     # system solved with q and r calculated by our function
-    x_householder = solve_system(r, q.transpose() @ b_init, 3)
-    print(x_householder)
+    x_householder = solve_householder(a_init.copy(), input_s)
+    print("X: ", x_householder)
 
     # system solved with q and r calculated by python lib
     q_lib, r_lib = np.linalg.qr(a_init)
-    print(q_lib)
-    print(r_lib)
+    print("Q lib\n", q_lib)
+    print("R lib\n", r_lib)
     xqr = solve_system(r_lib, q_lib.transpose() @ b_init, 3)
-    print(xqr)
+    print("XQR lib", xqr)
 
-    print(norm_check(xqr, x_householder))
+    print("Norm3: ", compute_norm(x_householder - xqr))
+    print("=====================================")
+
+    print("Exercise 4")
+    print("Norm41:", compute_norm(a_init @ x_householder - b_init.T[0]))
+    print("Norm42:", compute_norm(a_init @ xqr - b_init.T[0]))
+    print("Norm43:", compute_norm(x_householder - input_s.T[0]) / compute_norm(input_s.T[0]))
+    print("Norm44:", compute_norm(xqr - input_s.T[0]) / compute_norm(input_s.T[0]))
+
+    print("=====================================")
+    print("Exercise 5")
+    _a_inv = compute_inv(a_init)
+    print("Inverse of A:\n", _a_inv)
+    a_inv_lib = np.linalg.inv(a_init)
+    print("Inverse of A lib:\n", a_inv_lib)
+    print("Norm5:", np.linalg.norm(_a_inv - a_inv_lib))
+
+    print("=====================================")
+    print("Exercise 6")
+    size = 150
+    input_a_n = np.random.uniform(0, 100, size=(size, size))
+    input_s_n = np.random.uniform(0, 100, size=(size, 1))
+    input_b_n = get_b(input_a_n, input_s_n)
+
+    q_lib_n, r_lib_n = np.linalg.qr(input_a_n)
+    xqr_n = solve_system(r_lib_n, q_lib_n.transpose() @ input_b_n, len(input_a_n))
+    x_householder_n = solve_householder(input_a_n.copy(), input_s_n)
+
+    # print("X: ", x_householder_n)
+    # print("XQR: ", x_qr_n)
+    print("Norm6: ", compute_norm(x_householder_n - xqr_n) < 10 ** -6)
+    print("=====================================")
+
+
+
+
