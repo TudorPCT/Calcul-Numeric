125df908f5a5c28bf851d2ac016d3b97f0797833
TudorPCT <tcosmin.pasat@gmail.com>
2023-03-29 12:41:24 +0300

    tema 5 - continuare

--- tema_5/main.py
+++ tema_5/main.py
@@ -3,12 +3,45 @@ from tema_5.symmetrical_rare_matrix import generate_sym_rare_matrix, power_itera
 
 if __name__ == '__main__':
 
-    m_512_generated = generate_sym_rare_matrix(5)
+    # print("Exemplu video")
+    # m = RareMatrix(path='./surse/m_test.txt')
+    # result_1 = power_iteration(m)
+    # print("Lambda: ", result_1[0])
+    # # print("V: ", result_1[1])
+    # print("Nr iteratii: ", result_1[2])
+    #
+    # print("----------------------------------------------------")
+    #
+    # print("Exemplu generat random")
+    # m_512_generated = generate_sym_rare_matrix(700)
+    # result_2 = power_iteration(m_512_generated)
+    # print("Lambda: ", result_2[0])
+    # # print("V: ", result_2[1])
+    # print("Nr iteratii: ", result_2[2])
+    #
+    # print("----------------------------------------------------")
+    #
+    # print("Exemplu din fisier - 512")
+    # m_512 = RareMatrix(path='./surse/m_rar_sim_2023_512.txt')
+    # result_4 = power_iteration(m_512)
+    # print("Lambda: ", result_4[0])
+    # # print("V: ", result_4[1])
+    # print("Nr iteratii: ", result_4[2])
+    #
+    # print("----------------------------------------------------")
+    #
+    # print("Exemplu din fisier - 1024")
+    # m_1024 = RareMatrix(path='./surse/m_rar_sim_2023_1024.txt')
+    # result_5 = power_iteration(m_1024)
+    # print("Lambda: ", result_5[0])
+    # # print("V: ", result_5[1])
+    # print("Nr iteratii: ", result_5[2])
 
-    m_512 = RareMatrix(path='./surse/m_rar_sim_2023_512.txt')
-    m_1024 = RareMatrix(path='./surse/m_rar_sim_2023_1024.txt')
+    print("----------------------------------------------------")
+
+    print("Exemplu din fisier - 2023")
     m_2023 = RareMatrix(path='./surse/m_rar_sim_2023_2023.txt')
-
-    print(m_512_generated)
-    power_iteration(m_512_generated)
-
+    result_6 = power_iteration(m_2023)
+    print("Lambda: ", result_6[0])
+    # print("V: ", result_6[1])
+    print("Nr iteratii: ", result_6[2])
--- tema_5/symmetrical_rare_matrix.py
+++ tema_5/symmetrical_rare_matrix.py
@@ -9,9 +9,11 @@ from tema_4.RareMatrix import RareMatrix
 
 def generate_sym_rare_matrix(n):
     a = RareMatrix(size=n)
+    probability = 2 / n
+    count = 0
+
     for i in range(n):
         for j in range(i, n):
-            probability = 0.5
             if random.random() < probability:
                 if a.matrix.get(i) is None:
                     a.matrix[i] = {}
@@ -19,24 +21,48 @@ def generate_sym_rare_matrix(n):
                     a.matrix[j] = {}
                 a.matrix[i][j] = random.uniform(0, 1000)
                 a.matrix[j][i] = a.matrix[i][j]
+                count += 1
+    # print(count)
     return a
 
 
 def v_init(n):
-    v = np.zeros(n)
-    v[0] = random.uniform(0, 1 / n)
-    norm = 1 - v[0] ** 2
 
-    for i in range(n-1):
-        v[i] = random.uniform(0, math.sqrt(norm))
-        norm -= v[i] ** 2
-
-    v[n-1] = math.sqrt(norm)
+    v = np.random.rand(n, 1)
+    v = v / np.linalg.norm(v)
 
     return v
 
 
-def power_iteration(a: RareMatrix):
+def compute_w(a: RareMatrix, v):
+    w = np.zeros((a.size, 1))
+
+    for i in a.matrix:
+        for j in a.matrix[i]:
+            w[i] += a.matrix[i][j] * v[j][0]
+
+    return w
+
+
+def power_iteration(a: RareMatrix, epsilon=10**-9, kmax=1000000):
+
+    if not a.is_symmetrical():
+        raise Exception("Matrix is not symmetrical")
+
     v = v_init(a.size)
-    print(np.linalg.norm(v))
-    print(v)
+    w = compute_w(a, v)
+    r = (w.T @ v)[0][0]
+    k = 0
+    while True:
+        v = w / np.linalg.norm(w)
+        w = compute_w(a, v)
+        r = (w.T @ v)[0][0]
+        k += 1
+
+        if np.linalg.norm(w - r * v) <= a.size * epsilon or k > kmax:
+            break
+
+    if k > kmax:
+        print("Max iterations reached")
+    else:
+        return r, v, k
